4e39c0666914f32d469baf448ae90a9f36c8823b
Yana Krukovska <jana.krua@gmail.com>
2019-09-29 12:10:10 +0300

    - Worked on getting top songs - Added tabs for top songs of different period of time

---
+++ app/src/main/java/com/ritacle/mymusichistory/testutils/TopSongsStubService.java
@@ -0,0 +1,28 @@
+package com.ritacle.mymusichistory.testutils;
+
+import com.ritacle.mymusichistory.model.LastListen;
+import com.ritacle.mymusichistory.model.Listen;
+import com.ritacle.mymusichistory.model.ListenAmount;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class TopSongsStubService {
+
+    public List<ListenAmount> getSongs() {
+        List<ListenAmount> topSongs = new LinkedList<>();
+        topSongs.add(new ListenAmount("Freaking Me Out", "Ava Max", "Freaking Me Out", 50));
+        topSongs.add(new ListenAmount("Freaking Me Out", "Ava Max", "Freaking Me Out", 40));
+        topSongs.add(new ListenAmount("Freaking Me Out", "Ava Max", "Freaking Me Out", 30));
+        topSongs.add(new ListenAmount("Freaking Me Out", "Ava Max", "Freaking Me Out", 28));
+        topSongs.add(new ListenAmount("Freaking Me Out", "Ava Max", "Freaking Me Out", 25));
+        topSongs.add(new ListenAmount("Freaking Me Out", "Ava Max", "Freaking Me Out", 22));
+        topSongs.add(new ListenAmount("Freaking Me Out", "Ava Max", "Freaking Me Out", 22));
+        topSongs.add(new ListenAmount("Freaking Me Out", "Ava Max", "Freaking Me Out", 21));
+        topSongs.add(new ListenAmount("Freaking Me Out", "Ava Max", "Freaking Me Out", 19));
+        topSongs.add(new ListenAmount("Freaking Me Out", "Ava Max", "Freaking Me Out", 15));
+        topSongs.add(new ListenAmount("Freaking Me Out", "Ava Max", "Freaking Me Out", 11));
+        topSongs.add(new ListenAmount("Freaking Me Out", "Ava Max", "Freaking Me Out", 2));
+        return topSongs;
+    }
+}
