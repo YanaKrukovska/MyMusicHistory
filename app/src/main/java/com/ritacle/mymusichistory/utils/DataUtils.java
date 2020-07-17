b0c40018e2891b66616661040778a62676f4dbb3
YanaKrukovska <jana.krua@gmail.com>
2020-07-17 16:04:03 +0300

    - ListenRegistrar now sets UTC time to new listens instead of local

--- app/src/main/java/com/ritacle/mymusichistory/utils/DataUtils.java
+++ app/src/main/java/com/ritacle/mymusichistory/utils/DataUtils.java
@@ -7,26 +7,21 @@ import java.util.Locale;
 import java.util.TimeZone;
 import java.util.concurrent.TimeUnit;
 
-import static java.time.temporal.ChronoUnit.DAYS;
-
 public class DataUtils {
 
 
     public static String convertToTimeLabel(Date date) {
         Date currentDate = new Date();
 
-        // for timeDateOld zone shift
-        long listenDate = date.getTime() - 10800000;
-      //  long listenDate = date.getTime();
+        long listenDate = date.getTime();
         long currentDateMilliseconds = currentDate.getTime();
         long minutes = ((currentDateMilliseconds - listenDate) / 1000) / 60;
 
         Calendar calendar = Calendar.getInstance();
         calendar.setTime(date);
 
-
-        String timeYesterday = new SimpleDateFormat("hh:mm aa", Locale.ENGLISH).format(calendar.getTime() );
-        String timeDateOld = new SimpleDateFormat("MMMM dd, hh:mm aa", Locale.ENGLISH).format(calendar.getTime() );
+        String timeYesterday = new SimpleDateFormat("hh:mm aa", Locale.ENGLISH).format(calendar.getTime());
+        String timeDateOld = new SimpleDateFormat("MMMM dd, hh:mm aa", Locale.ENGLISH).format(calendar.getTime());
 
         if (minutes < 1) {
             return "Just now";
@@ -40,44 +35,44 @@ public class DataUtils {
             return "Yesterday " + timeYesterday;
         }
 
-
         return timeDateOld;
     }
 
+    public static Date createFullDate(int year, int month, int day, int hours, int minutes, int seconds) {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(year, month - 1, day, hours, minutes, seconds);
+        return calendar.getTime();
+    }
 
     public static Date createDate(int year, int month, int day) {
         TimeZone.setDefault(TimeZone.getTimeZone("CET"));
         Calendar calendar = Calendar.getInstance();
-        calendar.set(year, month-1, day, 0, 0, 0);
+        calendar.set(year, month - 1, day, 0, 0, 0);
         return calendar.getTime();
     }
 
-    public static String convertToString(Date date){
-
+    public static String convertToString(Date date) {
         Calendar calendar = Calendar.getInstance();
         calendar.setTime(date);
         return new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH).format(calendar.getTime());
     }
 
-    public static Date addDays(Date date, int days){
-
+    public static Date addDays(Date date, int days) {
         Calendar calendar = Calendar.getInstance();
         calendar.setTime(date);
         calendar.add(Calendar.DAY_OF_YEAR, days);
-
         return calendar.getTime();
     }
 
-    public static Date addMonth(Date date, int month){
-
+    public static Date addMonth(Date date, int month) {
         Calendar calendar = Calendar.getInstance();
         calendar.setTime(date);
         calendar.add(Calendar.MONTH, month);
-
         return calendar.getTime();
     }
 
-    public static int daysBetween(Date date1, Date date2){
+    public static int daysBetween(Date date1, Date date2) {
         long diff = date2.getTime() - date1.getTime();
         return (int) TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
     }
--- app/src/test/java/com/ritacle/mymusichistory/utils/DataUtilsTest.java
+++ app/src/test/java/com/ritacle/mymusichistory/utils/DataUtilsTest.java
@@ -3,16 +3,25 @@ package com.ritacle.mymusichistory.utils;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.text.DateFormat;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
 
-import static com.ritacle.mymusichistory.utils.DataUtils.*;
+import static com.ritacle.mymusichistory.utils.DataUtils.addDays;
+import static com.ritacle.mymusichistory.utils.DataUtils.addMonth;
+import static com.ritacle.mymusichistory.utils.DataUtils.convertToString;
+import static com.ritacle.mymusichistory.utils.DataUtils.convertToTimeLabel;
+import static com.ritacle.mymusichistory.utils.DataUtils.createDate;
 
 public class DataUtilsTest {
 
     @Test
     public void convertToTimeLabelJustNow() {
         Assert.assertEquals("Just now", convertToTimeLabel(new Date()));
-
     }
 
     @Test
@@ -23,18 +32,27 @@ public class DataUtilsTest {
     @Test
     public void convertToTimeLabelOld() {
 
-
     }
 
     @Test
     public void get7DaysFromNow() {
-
         Assert.assertEquals("2019-08-19", convertToString(addDays(createDate(2019, 8, 26), -7)));
     }
 
     @Test
     public void getOneMonthFromNow() {
-
         Assert.assertEquals("2019-07-26", convertToString(addMonth(createDate(2019, 8, 26), -1)));
+    }
+
+    @Test
+    public void compareApi26And23UTCTime() {
+        ZonedDateTime utc = ZonedDateTime.now(ZoneOffset.UTC);
+        utc.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        Date date26 = DataUtils.createFullDate(utc.getYear(), utc.getMonthValue(), utc.getDayOfMonth(), utc.getHour(), utc.getMinute(), utc.getSecond());
+
+        DateFormat dateFormat = DateFormat.getDateTimeInstance(1, 1, Locale.ENGLISH);
+        dateFormat.setTimeZone(TimeZone.getTimeZone("utc"));
+        Date date23 = dateFormat.getCalendar().getTime();
+        Assert.assertEquals(date26.getHours(), date23.getHours());
     }
 }
